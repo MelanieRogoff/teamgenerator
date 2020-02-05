f9c34beecd339f4169b73227312522b60be81a4f
Melanie Rogoff <mgrogoff@gmail.com>
2020-02-05 00:37:12 -0700

    Fixing

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,19 +2,20 @@ const Intern = require("./../lib/internclass");
 const answers = require("./../app");
 
 test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern(testValue, `${answers.name}`, 1, "test@test.com");
-  expect(e.school).toBe(testValue);
-});
-
-test("getRole() should return \"Intern\"", () => {
-  const testValue = "Intern";
-  const e = new Intern(`${answers.school}`, "Name", "1", `${answers.title}`);
-  expect(e.getRole()).toBe(testValue);
-});
-
-test("Can get school via getSchool()", () => {
-  const testValue = `${answers.school}`;
-  const e = new Intern("Foo", 1, "test@test.com", `${answers.school}`);
-  expect(e.getSchool()).toBe(`${answers.school}`);
-});
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+  });
+  
+  test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Can get school via getSchool()", () => {
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+  });
+  
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -2,19 +2,20 @@ const Manager = require("./../lib/managerclass");
 const answers = require("./../app");
 
 test("Can set office number via constructor argument", () => {
-  const testValue = `${answers.number}`;
-  const e = new Manager(`${answers.number}`, 1, "test@test.com", testValue);
-  expect(e.officeNumber).toBe(testValue);
-});
-
-test("getRole() should return \"Manager\"", () => {
-  const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
-  expect(e.getRole()).toBe(testValue);
-});
-
-test("Can get office number via getOffice()", () => {
-  const testValue = `${answers.number}`;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
-});
+    const testValue = 100;
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+  });
+  
+  test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Foo", 1, "test@test.com", 100);
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Can get office number via getOffice()", () => {
+    const testValue = 100;
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+  });
+  
