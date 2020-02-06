8fa6d85ac29061fd7e945edc840d3d774fca147c
Melanie Rogoff <mgrogoff@gmail.com>
2020-02-06 01:38:28 -0700

    Everything regarding getting manager/intern/engineer data now works.

--- app.js
+++ app.js
@@ -18,84 +18,51 @@ let duplicateCheck = false;  //flagging this variable in order to check if there
 
 let teamArray = [];
 
-const questions = ["Welcome! Let's build your team. To begin, please answer the following: What is your first name?", "What is your email?", "What is your ID?", "Are you an intern, manager, or engineer?"];
-
-
 function initialPrompt() {
 inquirer
     .prompt([{
             type: "checkbox",
-            message: questions[3],
+            message: "Let's build your team. To begin, please click on Manager to create the team manager.",
             name: "title",
-            choices: ["Intern", "Manager", "Engineer"]
+            choices: ["Manager"]
         },
         {
             type: "input",
-            message: questions[0],
+            message: "What is your manager's first name?",
             name: "name"
         },
         {
             type: "input",
-            message: questions[1],
+            message: "What is your manager's email address?",
             name: "email"
         },
         {
             type: "number",
-            message: questions[2],
+            message: "What is your manager's ID number?",
             name: "id"
         },
     ])
     .then(answers => {
-              //REGULAR IF STATEMENT CHECKS
-              if (answers.title == '' || answers.name == '' || answers.email == '' || answers.id == '' || answers.school == '' || answers.github == '' || answers.number == '') {
-                console.log("Please enter a value and start again.");
-            } 
-
         const employeeDetails = answers;
-        //INTERN CHECK
-        if (employeeDetails.title == 'Intern') {
-            inquirer.prompt([{
-                type: "input",
-                message: "What school do you go to?",
-                name: "school"
-                },
-            ])
-        .then(answers => {
-          intern = new Intern(employeeDetails.name, employeeDetails.id, employeeDetails.email, answers.school);
-          teamArray.push(intern);//push to teamArray
-          addUser();
-        })
-    };
-    //ENGINEER CHECK
-    if (employeeDetails.title == 'Engineer') {
-        inquirer.prompt([{
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "github"
-        }, 
-        ])
-        .then(answers => {
-            engineer = new Engineer(employeeDetails.name, employeeDetails.id, employeeDetails.email, answers.github);
-            teamArray.push(engineer)//push to teamArray
-            addUser();
-        })
-    }
-
-    //MANAGER CHECK + DUPLICATE CHECK
-        if (employeeDetails.title == 'Manager' && !duplicateCheck) {
+        if (employeeDetails.title == 'Manager') {
             inquirer.prompt([{
                 type: "number",
-                message: "What is your office number?",
+                message: "What is your manager's office number?",
                 name: "number"
             }, 
         ])
             .then(answers => {
+                let employeeDetails = answers; 
                 manager = new Manager(employeeDetails.name, employeeDetails.id, employeeDetails.email, answers.number);
                 teamArray.push(manager);// ONLY pushing if duplicateCheck is false
                 addUser();  
             })
        
-        //RIGHT NOW, MANAGER CAN STILL DUPLICATE. 
+
+              //REGULAR IF STATEMENT CHECKS
+              if (answers.title == '' || answers.name == '' || answers.email == '' || answers.id == '' || answers.school == '' || answers.github == '' || answers.number == '') {
+                console.log("Please enter a value and start again.");
+            } 
 
 
 
@@ -111,7 +78,9 @@ inquirer
                 ])
                 .then(answers => {
                         if (answers.addUser) {
-                            initialPrompt();
+                            chooser();  //function for adding engineer or intern
+                            //If chooser's result was engineer ...  
+                            //If chooser's result was intern ...
                         } else {
                             pdf.create(generateHTML(answers)).toFile('./output_htmls/team.pdf', function (err) {
                                 if (err) return console.log(err);
@@ -122,9 +91,83 @@ inquirer
                         }})
 
 
-
-
                     }
         
-        initialPrompt();
+        initialPrompt(); //calling initial prompt
     
+    function chooser() {
+        inquirer
+            .prompt([{
+                type: "checkbox",
+                message: "Please choose whether to add an Engineer, or an Intern.",
+                name: "choice",
+                choices: ["Engineer", "Intern",]
+            },
+            ])
+            .then (answers => {
+                //IF CHOOSING ENGINEER
+                if (answers.choice == "Engineer") {
+                    let employeeDetails = answers; 
+                        inquirer.prompt([{
+                            type: "input", 
+                            message: "What is the engineer's first name?",
+                            name: "engineerName"
+                        },
+                        {
+                            type: "input",
+                            message: "What is the engineer's GitHub username?",
+                            name: "github"
+                        },
+                        {
+                            type: "input",
+                            message: "What is the engineer's ID number?",
+                            name: "id"
+                        }, 
+                        {
+                        type: "input",
+                        message: "What is the engineer's email address?",
+                        name: "email"
+                        }, 
+                    ])
+                    .then(answers => {
+                        let employeeDetails = answers;
+                        engineer = new Engineer(employeeDetails.name, employeeDetails.id, employeeDetails.email, answers.github);
+                        teamArray.push(engineer)//push to teamArray
+                        addUser();
+                    })
+                }; 
+                    if (answers.choice == "Intern") {
+                            inquirer.prompt([{
+                                type: "input", 
+                                message: "What is the intern's first name?",
+                                name: "internName"
+                            },
+                            {
+                                type: "input",
+                                message: "What school does the intern attend?",
+                                name: "school"
+                            },
+                            {
+                                type: "input",
+                                message: "What is the intern's ID number?",
+                                name: "id"
+                            }, 
+                            {
+                                type: "input",
+                                message: "What is the intern's email address?",
+                                name: "email"
+                            }, 
+                        ])
+                        .then(answers => {
+                            let employeeDetails = answers;
+                            intern = new Intern(employeeDetails.name, employeeDetails.id, employeeDetails.email, answers.github);
+                            teamArray.push(intern)//push to teamArray
+                            addUser();
+                        })
+
+                    }
+                }
+                )}
+                   
+               
+                       
